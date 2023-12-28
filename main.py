98e68d86e03a4e12e42f3ad062de665a19f46715
Justin Triplett <justin.triplett@gmail.com>
2023-12-28 15:00:26 -0500

    add get_account_data and get_account_statuses

--- main.py
+++ main.py
@@ -17,13 +17,26 @@ def write_account_data(account):
         f.write(json.dumps(account, default=datetime_serializer, indent=4))
 
 
+def get_account_data(client, account_id=None):
+    """Get account data from API, default to client user if no account_id given."""
+    if account_id:
+        account_data = client.account(account_id)
+    else:
+        account_data = client.me()
+    return account_data
+
+
+def get_account_statuses(client, account_id=None):
+    """Get account statuses from API, default to client user if no account_id given."""
+    if account_id:
+        statuses = client.account_statuses(account_id)
+    else:
+        statuses = client.account_statuses(client.me()['id'])
+    return statuses
+
+
 def post_msg(client, msg):
     client.toot(msg)
-
-
-def main(config):
-    mastodon_client = create_poster_client(config)
-    post_msg(mastodon_client, 'API POSTER IS WORKING')
 
 
 def load_config():
@@ -36,6 +49,11 @@ def create_poster_client(config):
         api_base_url=config['api_base_url'],
         access_token=config['poster-creds']['access_token'],
     )
+
+
+def main(config):
+    mastodon_client = create_poster_client(config)
+    post_msg(mastodon_client, 'API POSTER IS WORKING')
 
 
 if __name__ == '__main__':
